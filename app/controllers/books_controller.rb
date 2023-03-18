5d62cfa92ad752f3b770e67bbc9d297c0a8df63b
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-03-18 08:36:00 +0000

    [Update] new、createアクションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -9,5 +19,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
