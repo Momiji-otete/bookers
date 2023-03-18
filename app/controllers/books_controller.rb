428efcc800d494c15d51624b852a38ec2b6f1066
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-03-18 12:18:10 +0000

    [Update] indexページ内に新規投稿フォームを移植

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,12 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
